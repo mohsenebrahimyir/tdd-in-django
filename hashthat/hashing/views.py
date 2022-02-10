a239660bc940e7b6268ffc62dc9d8c9c88b353f4
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-02-10 19:03:25 +0330

    013-saving your model

--- hashthat/hashing/views.py
+++ hashthat/hashing/views.py
@@ -1,11 +1,24 @@
-from multiprocessing import context
+from cgitb import text
+from distutils import text_file
 from django.shortcuts import render
 from .forms import HashForm
- 
+from .models import Hash
+import hashlib
 
 def home(request):
+    if request.method == "POST":
+        filled_form = HashForm(request.POST)
+        if filled_form.is_valid():
+            text = filled_form.cleaned_data['text']
+            text_hash = hashlib.sha256(text.encode('utf-8')).hexdigest() 
+            try:
+                Hash.objects.get(hash=text_hash)
+            except Hash.DoesNotExist:
+                hash = Hash()
+                hash.text = text
+                hash.hash = text_hash
+                hash.save()
+            
     form = HashForm()
-    context = {
-        'form': form,
-    }
+    context = {'form': form}
     return render(request, 'hashing/home.html', context)
